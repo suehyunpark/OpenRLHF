13fc3213b55866cd3ff69b8835e94c51352d9e5c
suehyunpark <shparksue@gmail.com>
2024-05-01 14:56:05 +0000

    Update rejection sampling

--- examples/scripts/mpa/train_rejection_sampling.sh
+++ examples/scripts/mpa/train_rejection_sampling.sh
@@ -3,6 +3,8 @@ set -x
 mkdir -p ./ckpt/mpa/7b_mistral_66k_rs
 mkdir -p ./log/mpa
 
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
 GENERATE_OUTPUT=./ckpt/mpa/7b_mistral_66k_rs/generate.jsonl
 RM_OUTPUT=./ckpt/mpa/7b_mistral_66k_rs/rm.jsonl
 MODEL_OUTPUT_PATH=./ckpt/mpa/7b_mistral_66k_rs
@@ -16,6 +18,12 @@ REWARD_MODEL_PATH="kaist-ai/mpa-Mistral-7b-v0.2-hf-rm-66k"
 DATASET_PATH="/mnt/nas/suehyun/MPA/data/train/preferences_v1_responses_for_orpo_64k_v2_ppo_mistral_input.jsonl"
 
 BEST_OF=4
+
+WANDB_API_KEY="339cad8697ca8b7558010d3f8c4aa40788e64d12"
+WANDB_ENTITY="suehyun"
+WANDB_PROJECT="mpa-rm"
+WANDB_RUN_NAME="mpa-Mistral-7b-v0.2-hf-rs-66k"
+
 
 checkSuccess() {
     if [[ $? != 0 ]]; then
@@ -46,8 +54,9 @@ while (($iter < $TRAINING_ITERS)); do
     --dataset $DATASET_PATH  \
     --input_key prompt \
     --dataset_probs 0.5,0.5 \
-    --temperature 0.9
-    --tp_size 8
+    --temperature 0.9 \
+    --flash_attn \
+    --tp_size 8 \
     --best_of_n $BEST_OF \
     --iter $iter \
     --rollout_batch_size $ROLLOUT_BATCH_SIZE \
@@ -66,6 +75,7 @@ EOF
     --dataset $GENERATE_OUTPUT  \
     --dataset_probs 1.0 \
     --zero_stage 0 \
+    --flash_attn \
     --post_processor rs \
     --micro_batch_size 4 \
     --output_path $RM_OUTPUT
@@ -88,8 +98,12 @@ EOF
     --max_epochs 2 \
     --bf16 \
     --learning_rate 2e-6 \
-    --gradient_checkpointing
-EOF
+    --gradient_checkpointing \
+    --flash_attn \
+    --use_wandb $WANDB_API_KEY \
+    --wandb_org $WANDB_ENTITY \
+    --wandb_project $WANDB_PROJECT \
+    --wandb_run_name $WANDB_RUN_NAME \
     echo $sft_commands
     deepspeed $sft_commands
     checkSuccess "SFT"
