3ad8ed29e68d641d1a342b56faff31b226a53380
suehyunpark <shparksue@gmail.com>
2024-05-05 13:10:20 +0000

    Update final ppo config

--- examples/scripts/train_ppo_llama.sh
+++ examples/scripts/train_ppo_llama.sh
@@ -9,9 +9,9 @@ read -r -d '' training_commands <<EOF
     --logging_steps 1 \
     --eval_steps -1 \
     --micro_train_batch_size 2 \
-    --train_batch_size 128 \
+    --train_batch_size 64 \
     --micro_rollout_batch_size 4 \
-    --rollout_batch_size 1024 \
+    --rollout_batch_size 512 \
     --max_epochs 1 \
     --prompt_max_len 1024 \
     --generate_max_len 1024 \
